eec1831935790daff419a40237af434745acc836
akorneva-ai <a.korneva@g.nsu.ru>
2026-02-28 18:53:21 +0700

    Update задание_5.py

--- задание_5.py
+++ задание_5.py
@@ -1,12 +1,17 @@
-s = 'ututututfddfs'
-t = 'otiyryjhgf'
-r = 'nmcvbniigjh'
-k = []
-for i in s + t + r:
-    if s.count(i) > 0 and (t + r).count(i) == 0:
-        k.append(i)
-    elif t.count(i) > 0 and (r + s).count(i) == 0:
-        k.append(i)
-    elif (s + t).count(i) == 0 and r.count(i) > 0:
-        k.append(i)
-print(set(k))
+line_1 = input()
+line_2 = input()
+line_3 = input()
+
+result = []
+
+for symbol in line_1 + line_2 + line_3:
+    if line_1.count(symbol) > 0 and (line_2 + line_3).count(symbol) == 0:
+        result.append(symbol)
+    elif line_2.count(symbol) > 0 and (line_3 + line_1).count(symbol) == 0:
+        result.append(symbol)
+    elif line_3.count(symbol) > 0 and (line_1 + line_2).count(symbol) == 0:
+        result.append(symbol)
+
+#set (множество) - это тип данных 
+#для хранения уникальных элементов (без повторений
+print(set(result))
