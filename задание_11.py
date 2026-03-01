c7211bd01ab3dd0e88828bc2e443a9dfb7cabdd8
akorneva-ai <a.korneva@g.nsu.ru>
2026-03-01 18:11:15 +0700

    Update задание_11.py

--- задание_11.py
+++ задание_11.py
@@ -1,11 +1,11 @@
-text = 'Berlin Novgorod Demichev Vologda Anadir Ravioli Ilotsk Kirov Voroneg Go'
+city = input()
 
-turn = 0
-words = text.lower().split()
-print(len(words))
-for i in range(1, len(words)):
+turn = 0 #кол-во пар городов
+towns = city.lower().split()
+
+for word in range(1, len(towns)):
     turn += 1
-    if words[i][0] == words[i-1][-1]:
+    if towns[word][0] == towns[word - 1][-1]:
         pass
     else:
         turn -= 1
@@ -14,4 +14,3 @@ if turn % 2 == 0:
     print('Петя выиграл')
 else:
     print('Вася выиграл')
-print(turn)
