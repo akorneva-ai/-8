e02009751c8da71ded5111e0228e7d86ae89fba0
akorneva-ai <a.korneva@g.nsu.ru>
2026-03-02 19:42:11 +0700

    Update задание_14.py

--- задание_14.py
+++ задание_14.py
@@ -1,27 +1,45 @@
-print('Ведущий вводит две строки: подсказку и загаданное слово.')
-hint = str(input())
-word = str(input())
-print('\n' * 25)
-turn = 0
-guess = '*' * len(word)
+print("Ведущий вводит две строки: подсказку и загаданное слово.")
+
+hint = input()
+hiden_word = str(input())
+guess = len(hiden_word) * "*"
+
+print("\n" * 25)
+print(hint, "\n", guess, "\n",
+      "Буква или слово (0 - буква, 1 - слово)?")
+
+player_input = int(input())
+count = 0
+
 while True:
-    turn += 1
-    print(hint)
-    print(guess)
-    switch = int(input('Буква или слово (0 - буква, 1 - слово)?'))
-    if switch == 0:
-        bukva = str(input())
-        if word.count(bukva) > 0:
-            for i in range(len(word)):
-                if word[i] == bukva:
-                    guess = guess[:i] + bukva + guess[i+1:]
-    else:
-        wordguess = str(input())
-        if wordguess == word:
-            print('Победа!')
-            break
-        else:
-            print('Проигрыш!')
-    if turn == 10:
-        print('Проигрыш!')
+    if player_input == 0:
+        letter = input()
+        found = False
+        count += 1
+
+        for l in range(len(hiden_word)):
+            if count <= 10:
+                if hiden_word[l] == letter:
+                    guess = guess[:l] + letter + guess[l + 1:]
+                    found = True
+                else:
+                    found = False
+
+    if count > 10:
+        print("Проигрыш!")
         break
+
+    print(guess, "\n", "Буква или слово (0 - буква, 1 - слово)?")
+    player_input = int(input())
+
+    if player_input == 1:
+        word = input()
+        if count <= 10:
+            if word == hiden_word:
+                print("Победа!")
+            else:
+                print("Проигрыш!")
+        else:
+            print("Проигрыш!")
+
+    break
