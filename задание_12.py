34e21996a641fea3d41cc19a577501be06431c7d
akorneva-ai <a.korneva@g.nsu.ru>
2026-03-02 10:23:34 +0700

    Update задание_12.py

--- задание_12.py
+++ задание_12.py
@@ -1,13 +1,10 @@
-import keyword as k
+import keyword
+name = input()
 
-def check(name):
-    if k.iskeyword(name):
-        return False
-    return name.isidentifier()
-
-name = str(input('Введите предпологаемое имя: '))
-
-if check(name):
-    print(f'{name} - допустимое имя в Python')
+#keyword -> ключевые слова
+# .iskeyword() -> проверяет, не является ли имя зарезервированным
+#.isidentifier -> проверяет все правила имен
+if not keyword.iskeyword(name) and name.isidentifier():
+    print(f"{name} допустимое имя в Python)
 else:
-    print(f'{name} - недопустимое имя в Python')
+    print(f"{name} - недопустимое имя в Python")
